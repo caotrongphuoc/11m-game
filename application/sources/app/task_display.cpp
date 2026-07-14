43441cedc9cc3dd952245cd189f5f046e4c1899a
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 23:58:48 +0700

    [UPDATE] wire game screen display signals

--- application/sources/app/task_display.cpp
+++ application/sources/app/task_display.cpp
@@ -9,8 +9,35 @@
 #include "task_list.h"
 #include "task_display.h"
 
+#include "screens.h"
+
 scr_mng_t scr_mng_app;
 
 void task_display(ak_msg_t* msg) {
+	switch (msg->sig) {
+	case EM_GAME_DISPLAY_SHOW_MENU: {
+		APP_DBG_SIG("EM_GAME_DISPLAY_SHOW_MENU\n");
+		SCREEN_TRAN(scr_game_menu_handle, &scr_game_menu);
+	} break;
+
+	case EM_GAME_DISPLAY_SHOW_PENALTY: {
+		APP_DBG_SIG("EM_GAME_DISPLAY_SHOW_PENALTY\n");
+		SCREEN_TRAN(scr_game_penalty_handle, &scr_game_penalty);
+	} break;
+
+	case EM_GAME_DISPLAY_SHOW_GAME_OVER: {
+		APP_DBG_SIG("EM_GAME_DISPLAY_SHOW_GAME_OVER\n");
+		SCREEN_TRAN(scr_game_over_handle, &scr_game_over);
+	} break;
+
+	case EM_GAME_DISPLAY_REFRESH: {
+		APP_DBG_SIG("EM_GAME_DISPLAY_REFRESH\n");
+		return;
+	}
+
+	default:
+		break;
+	}
+
 	scr_mng_dispatch(msg);
 }
