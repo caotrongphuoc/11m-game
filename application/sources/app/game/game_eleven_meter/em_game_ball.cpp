818416c72cbde92ec4dc78e3b1b7edd9d6e1013e
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 23:43:40 +0700

    [REMOVE] clean orphaned fsm includes from em_game tasks

--- application/sources/app/game/game_eleven_meter/em_game_ball.cpp
+++ application/sources/app/game/game_eleven_meter/em_game_ball.cpp
@@ -1,4 +1,4 @@
-#include "fsm.h"
+#include "ak.h"
 #include "port.h"
 #include "message.h"
 
--- application/sources/app/game/game_eleven_meter/em_game_goal.cpp
+++ application/sources/app/game/game_eleven_meter/em_game_goal.cpp
@@ -1,6 +1,6 @@
-#include "fsm.h"
-#include "message.h"
+#include "ak.h"
 #include "port.h"
+#include "message.h"
 
 #include "em_game_goal.h"
 
