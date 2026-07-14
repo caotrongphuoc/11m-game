5850e9617f92b0354f74db2ea7b9dd8138b40092
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 23:30:19 +0700

    [UPDATE] refactor em_game match state handling

--- application/sources/app/game/game_eleven_meter/em_game_match.cpp
+++ application/sources/app/game/game_eleven_meter/em_game_match.cpp
@@ -1,4 +1,3 @@
-#include "fsm.h"
 #include "port.h"
 #include "message.h"
 #include "timer.h"
@@ -14,12 +13,17 @@
 static em_game_match_state_t s_match;
 static bool s_match_initialized = false;
 
-static void em_game_match_state_menu(ak_msg_t* msg);
-static void em_game_match_state_round_start(ak_msg_t* msg);
-static void em_game_match_state_shooter_wait(ak_msg_t* msg);
-static void em_game_match_state_reveal(ak_msg_t* msg);
-static void em_game_match_state_round_end(ak_msg_t* msg);
-static void em_game_match_state_game_over(ak_msg_t* msg);
+enum
+{
+	STATE_MENU,
+	STATE_ROUND_START,
+	STATE_SHOOTER_WAIT,
+	STATE_REVEAL,
+	STATE_ROUND_END,
+	STATE_GAME_OVER,
+};
+
+static uint8_t s_state;
 
 static void em_game_match_handle_hit_result(ak_msg_t* msg)
 {
@@ -55,7 +59,7 @@ static void em_game_match_handle_hit_result(ak_msg_t* msg)
 	task_post_pure_msg(AC_TASK_BUZZER_ID, buzzer_signal);
 	em_game_scoreboard_record_result(&s_match.scoreboard, result);
 	timer_set(EM_GAME_MATCH_ID, EM_GAME_MATCH_ROUND_END, EM_GAME_MATCH_ROUND_END_INTERVAL, TIMER_ONE_SHOT);
-	FSM_TRAN(&s_match, em_game_match_state_round_end);
+	s_state = STATE_ROUND_END;
 }
 
 static void em_game_match_finish_match()
@@ -73,7 +77,7 @@ static void em_game_match_finish_match()
 		task_post_pure_msg(AC_TASK_BUZZER_ID, EM_GAME_BUZZER_LOSE);
 	}
 
-	FSM_TRAN(&s_match, em_game_match_state_game_over);
+	s_state = STATE_GAME_OVER;
 }
 
 static void em_game_match_start_kick(em_game_direction_t direction)
@@ -87,145 +91,7 @@ static void em_game_match_start_kick(em_game_direction_t direction)
 	task_post_common_msg(EM_GAME_BALL_ID, EM_GAME_BALL_KICK, &direction_payload, sizeof(direction_payload));
 	task_post_pure_msg(AC_TASK_BUZZER_ID, EM_GAME_BUZZER_KICK);
 
-	FSM_TRAN(&s_match, em_game_match_state_reveal);
-}
-
-static void em_game_match_state_menu(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_INPUT_CENTER:
-		em_game_scoreboard_reset(&s_match.scoreboard);
-		FSM_TRAN(&s_match, em_game_match_state_round_start);
-		task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_SETUP);
-		break;
-
-	default:
-		break;
-	}
-}
-
-static void em_game_match_state_round_start(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_SETUP:
-		em_game_scoreboard_advance_round(&s_match.scoreboard);
-		s_match.ball.direction = EM_GAME_DIRECTION_NONE;
-		s_match.keeper.direction = EM_GAME_DIRECTION_NONE;
-		s_match.shooter.direction = EM_GAME_DIRECTION_NONE;
-		s_match.pending_direction = EM_GAME_DIRECTION_NONE;
-		s_match.last_result = EM_GAME_RESULT_NONE;
-		task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_START_ROUND);
-		break;
-
-	case EM_GAME_MATCH_START_ROUND:
-		s_match.countdown_start_tick = sys_ctrl_millis();
-		s_match.countdown_seconds = 3;
-		task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_PENALTY);
-		timer_set(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT, EM_GAME_MATCH_SELECTION_TIMEOUT, TIMER_ONE_SHOT);
-		FSM_TRAN(&s_match, em_game_match_state_shooter_wait);
-		break;
-
-	default:
-		break;
-	}
-}
-
-static void em_game_match_state_shooter_wait(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_INPUT_LEFT:
-		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
-		em_game_match_start_kick(EM_GAME_DIRECTION_LEFT);
-		break;
-
-	case EM_GAME_MATCH_INPUT_CENTER:
-		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
-		em_game_match_start_kick(EM_GAME_DIRECTION_CENTER);
-		break;
-
-	case EM_GAME_MATCH_INPUT_RIGHT:
-		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
-		em_game_match_start_kick(EM_GAME_DIRECTION_RIGHT);
-		break;
-
-	case EM_GAME_MATCH_SHOOTER_TIMEOUT:
-		em_game_match_start_kick(EM_GAME_DIRECTION_CENTER);
-		break;
-
-	case EM_GAME_MATCH_RESET:
-		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
-		FSM_TRAN(&s_match, em_game_match_state_menu);
-		break;
-
-	default:
-		break;
-	}
-}
-
-static void em_game_match_state_reveal(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_HIT_RESULT:
-		em_game_match_handle_hit_result(msg);
-		break;
-
-	case EM_GAME_MATCH_RESET:
-		FSM_TRAN(&s_match, em_game_match_state_menu);
-		break;
-
-	default:
-		break;
-	}
-}
-
-static void em_game_match_state_round_end(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_ROUND_END:
-		if (em_game_scoreboard_is_complete(&s_match.scoreboard))
-		{
-			em_game_match_finish_match();
-		}
-		else
-		{
-			FSM_TRAN(&s_match, em_game_match_state_round_start);
-			task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_SETUP);
-		}
-		break;
-
-	case EM_GAME_MATCH_RESET:
-		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_ROUND_END);
-		FSM_TRAN(&s_match, em_game_match_state_menu);
-		break;
-
-	default:
-		break;
-	}
-}
-
-static void em_game_match_state_game_over(ak_msg_t* msg)
-{
-	switch (msg->sig)
-	{
-	case EM_GAME_MATCH_INPUT_CENTER:
-		em_game_match_state_reset(&s_match);
-		FSM_TRAN(&s_match, em_game_match_state_menu);
-		task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_MENU);
-		break;
-
-	case EM_GAME_MATCH_INPUT_LEFT:
-		FSM_TRAN(&s_match, em_game_match_state_menu);
-		task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_MENU);
-		break;
-
-	default:
-		break;
-	}
+	s_state = STATE_REVEAL;
 }
 
 void em_game_match_handle(ak_msg_t* msg)
@@ -233,10 +99,125 @@ void em_game_match_handle(ak_msg_t* msg)
 	if (!s_match_initialized)
 	{
 		em_game_match_state_init(&s_match);
-		FSM(&s_match, em_game_match_state_menu);
+		s_state = STATE_MENU;
 		task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_MENU);
 		s_match_initialized = true;
 	}
 
-	fsm_dispatch((fsm_t*)&s_match, msg);
+	switch (msg->sig)
+	{
+	case EM_GAME_MATCH_SETUP:
+		if (s_state == STATE_ROUND_START)
+		{
+			em_game_scoreboard_advance_round(&s_match.scoreboard);
+			s_match.ball.direction = EM_GAME_DIRECTION_NONE;
+			s_match.keeper.direction = EM_GAME_DIRECTION_NONE;
+			s_match.shooter.direction = EM_GAME_DIRECTION_NONE;
+			s_match.pending_direction = EM_GAME_DIRECTION_NONE;
+			s_match.last_result = EM_GAME_RESULT_NONE;
+			task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_START_ROUND);
+		}
+		break;
+
+	case EM_GAME_MATCH_START_ROUND:
+		if (s_state == STATE_ROUND_START)
+		{
+			s_match.countdown_start_tick = sys_ctrl_millis();
+			s_match.countdown_seconds = 3;
+			task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_PENALTY);
+			timer_set(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT, EM_GAME_MATCH_SELECTION_TIMEOUT, TIMER_ONE_SHOT);
+			s_state = STATE_SHOOTER_WAIT;
+		}
+		break;
+
+	case EM_GAME_MATCH_INPUT_LEFT:
+		if (s_state == STATE_SHOOTER_WAIT)
+		{
+			timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
+			em_game_match_start_kick(EM_GAME_DIRECTION_LEFT);
+		}
+		else if (s_state == STATE_GAME_OVER)
+		{
+			s_state = STATE_MENU;
+			task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_MENU);
+		}
+		break;
+
+	case EM_GAME_MATCH_INPUT_CENTER:
+		if (s_state == STATE_MENU)
+		{
+			em_game_scoreboard_reset(&s_match.scoreboard);
+			s_state = STATE_ROUND_START;
+			task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_SETUP);
+		}
+		else if (s_state == STATE_SHOOTER_WAIT)
+		{
+			timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
+			em_game_match_start_kick(EM_GAME_DIRECTION_CENTER);
+		}
+		else if (s_state == STATE_GAME_OVER)
+		{
+			em_game_match_state_reset(&s_match);
+			s_state = STATE_MENU;
+			task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_MENU);
+		}
+		break;
+
+	case EM_GAME_MATCH_INPUT_RIGHT:
+		if (s_state == STATE_SHOOTER_WAIT)
+		{
+			timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
+			em_game_match_start_kick(EM_GAME_DIRECTION_RIGHT);
+		}
+		break;
+
+	case EM_GAME_MATCH_SHOOTER_TIMEOUT:
+		if (s_state == STATE_SHOOTER_WAIT)
+		{
+			em_game_match_start_kick(EM_GAME_DIRECTION_CENTER);
+		}
+		break;
+
+	case EM_GAME_MATCH_HIT_RESULT:
+		if (s_state == STATE_REVEAL)
+		{
+			em_game_match_handle_hit_result(msg);
+		}
+		break;
+
+	case EM_GAME_MATCH_ROUND_END:
+		if (s_state == STATE_ROUND_END)
+		{
+			if (em_game_scoreboard_is_complete(&s_match.scoreboard))
+			{
+				em_game_match_finish_match();
+			}
+			else
+			{
+				s_state = STATE_ROUND_START;
+				task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_SETUP);
+			}
+		}
+		break;
+
+	case EM_GAME_MATCH_RESET:
+		if (s_state == STATE_SHOOTER_WAIT)
+		{
+			timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_SHOOTER_TIMEOUT);
+			s_state = STATE_MENU;
+		}
+		else if (s_state == STATE_REVEAL)
+		{
+			s_state = STATE_MENU;
+		}
+		else if (s_state == STATE_ROUND_END)
+		{
+			timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_ROUND_END);
+			s_state = STATE_MENU;
+		}
+		break;
+
+	default:
+		break;
+	}
 }
