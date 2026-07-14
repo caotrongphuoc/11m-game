0ef09d8accebbc1473a75c922d7727c08aad6c46
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 22:30:45 +0700

    [ADD] initialize em_game match state

--- application/sources/app/game/game_eleven_meter/em_game_match.cpp
+++ application/sources/app/game/game_eleven_meter/em_game_match.cpp
@@ -1,7 +1,20 @@
 #include "fsm.h"
-#include "message.h"
 #include "port.h"
+#include "message.h"
 
 #include "em_game_match.h"
+#include "em_game_match_state.h"
 
-void em_game_match_handle(ak_msg_t *msg) { (void)msg; }
+static em_game_match_state_t s_match;
+static bool s_match_initialized = false;
+
+void em_game_match_handle(ak_msg_t* msg)
+{
+	if (!s_match_initialized)
+	{
+		em_game_match_state_init(&s_match);
+		s_match_initialized = true;
+	}
+
+	(void)msg;
+}
