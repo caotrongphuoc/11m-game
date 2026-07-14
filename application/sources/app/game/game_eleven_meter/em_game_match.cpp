d0901319c5b8d0866d6772773d75348d64285a74
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 23:03:50 +0700

    [ADD] resolve em_game round results

--- application/sources/app/game/game_eleven_meter/em_game_match.cpp
+++ application/sources/app/game/game_eleven_meter/em_game_match.cpp
@@ -21,6 +21,61 @@ static void em_game_match_state_reveal(ak_msg_t* msg);
 static void em_game_match_state_round_end(ak_msg_t* msg);
 static void em_game_match_state_game_over(ak_msg_t* msg);
 
+static void em_game_match_handle_hit_result(ak_msg_t* msg)
+{
+	em_game_result_t result;
+	uint8_t buzzer_signal;
+
+	if (get_data_len_common_msg(msg) != sizeof(uint8_t))
+	{
+		return;
+	}
+
+	result = (em_game_result_t)(*get_data_common_msg(msg));
+
+	switch (result)
+	{
+	case EM_GAME_RESULT_GOAL:
+		buzzer_signal = EM_GAME_BUZZER_GOAL;
+		break;
+
+	case EM_GAME_RESULT_SAVE:
+		buzzer_signal = EM_GAME_BUZZER_SAVE;
+		break;
+
+	case EM_GAME_RESULT_MISS:
+		buzzer_signal = EM_GAME_BUZZER_MISS;
+		break;
+
+	default:
+		return;
+	}
+
+	s_match.last_result = result;
+	task_post_pure_msg(AC_TASK_BUZZER_ID, buzzer_signal);
+	em_game_scoreboard_record_result(&s_match.scoreboard, result);
+	timer_set(EM_GAME_MATCH_ID, EM_GAME_MATCH_ROUND_END, EM_GAME_MATCH_ROUND_END_INTERVAL, TIMER_ONE_SHOT);
+	FSM_TRAN(&s_match, em_game_match_state_round_end);
+}
+
+static void em_game_match_finish_match()
+{
+	em_game_winner_t winner = em_game_scoreboard_evaluate_winner(&s_match.scoreboard);
+
+	task_post_pure_msg(AC_TASK_DISPLAY_ID, EM_GAME_DISPLAY_SHOW_GAME_OVER);
+
+	if (winner == EM_GAME_WINNER_PLAYER)
+	{
+		task_post_pure_msg(AC_TASK_BUZZER_ID, EM_GAME_BUZZER_WIN);
+	}
+	else
+	{
+		task_post_pure_msg(AC_TASK_BUZZER_ID, EM_GAME_BUZZER_LOSE);
+	}
+
+	FSM_TRAN(&s_match, em_game_match_state_game_over);
+}
+
 static void em_game_match_start_kick(em_game_direction_t direction)
 {
 	uint8_t direction_payload = (uint8_t)direction;
@@ -112,12 +167,45 @@ static void em_game_match_state_shooter_wait(ak_msg_t* msg)
 
 static void em_game_match_state_reveal(ak_msg_t* msg)
 {
-	(void)msg;
+	switch (msg->sig)
+	{
+	case EM_GAME_MATCH_HIT_RESULT:
+		em_game_match_handle_hit_result(msg);
+		break;
+
+	case EM_GAME_MATCH_RESET:
+		FSM_TRAN(&s_match, em_game_match_state_menu);
+		break;
+
+	default:
+		break;
+	}
 }
 
 static void em_game_match_state_round_end(ak_msg_t* msg)
 {
-	(void)msg;
+	switch (msg->sig)
+	{
+	case EM_GAME_MATCH_ROUND_END:
+		if (em_game_scoreboard_is_complete(&s_match.scoreboard))
+		{
+			em_game_match_finish_match();
+		}
+		else
+		{
+			FSM_TRAN(&s_match, em_game_match_state_round_start);
+			task_post_pure_msg(EM_GAME_MATCH_ID, EM_GAME_MATCH_SETUP);
+		}
+		break;
+
+	case EM_GAME_MATCH_RESET:
+		timer_remove_attr(EM_GAME_MATCH_ID, EM_GAME_MATCH_ROUND_END);
+		FSM_TRAN(&s_match, em_game_match_state_menu);
+		break;
+
+	default:
+		break;
+	}
 }
 
 static void em_game_match_state_game_over(ak_msg_t* msg)
