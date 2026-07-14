9719ec08c507d9339f0cd1b4eaf2db44cd640310
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-14 22:26:29 +0700

    [ADD] em_game match state and view snapshot

---
+++ application/sources/app/game/game_eleven_meter/em_game_match_state.cpp
@@ -0,0 +1,37 @@
+#include <string.h>
+
+#include "em_game_match_state.h"
+
+void em_game_match_state_init(em_game_match_state_t* ms)
+{
+	memset(ms, 0, sizeof(em_game_match_state_t));
+	em_game_scoreboard_reset(&ms->scoreboard);
+}
+
+void em_game_match_state_reset(em_game_match_state_t* ms)
+{
+	uint32_t random_seed = ms->random_seed;
+
+	em_game_match_state_init(ms);
+	ms->random_seed = random_seed;
+}
+
+void em_game_match_state_build_view(const em_game_match_state_t* ms, em_game_view_t* view)
+{
+	memset(view, 0, sizeof(em_game_view_t));
+
+	view->round = ms->scoreboard.round;
+	view->goals = ms->scoreboard.goals;
+	view->saves = ms->scoreboard.saves;
+	view->misses = ms->scoreboard.misses;
+	view->countdown = ms->countdown_seconds;
+	view->shooter_direction = (uint8_t)ms->shooter.direction;
+	view->keeper_direction = (uint8_t)ms->keeper.direction;
+	view->last_result = (uint8_t)ms->last_result;
+	view->winner = (uint8_t)ms->scoreboard.winner;
+	view->ball_x = ms->ball.x;
+	view->ball_y = ms->ball.y;
+	view->ball_frame = ms->ball.frame;
+	view->keeper_frame = ms->keeper.frame;
+	view->shooter_frame = ms->shooter.frame;
+}
---
+++ application/sources/app/game/game_eleven_meter/em_game_match_state.h
@@ -0,0 +1,87 @@
+#ifndef __EM_GAME_MATCH_STATE_H__
+#define __EM_GAME_MATCH_STATE_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "fsm.h"
+#include "port.h"
+#include "message.h"
+
+#include "em_game_types.h"
+#include "em_game_scoreboard.h"
+
+typedef struct
+{
+	int16_t x;
+	int16_t y;
+	int16_t target_x;
+	int16_t target_y;
+	uint8_t frame;
+	bool visible;
+	bool moving;
+	bool wide;
+	em_game_direction_t direction;
+} em_game_ball_t;
+
+typedef struct
+{
+	int16_t x;
+	int16_t y;
+	uint8_t frame;
+	bool visible;
+	em_game_direction_t direction;
+} em_game_keeper_t;
+
+typedef struct
+{
+	int16_t x;
+	int16_t y;
+	uint8_t frame;
+	bool visible;
+	em_game_direction_t direction;
+} em_game_shooter_t;
+
+typedef struct
+{
+	fsm_t fsm;
+	em_game_ball_t ball;
+	em_game_keeper_t keeper;
+	em_game_shooter_t shooter;
+	em_game_scoreboard_t scoreboard;
+	em_game_direction_t pending_direction;
+	em_game_result_t last_result;
+	uint32_t random_seed;
+	uint32_t countdown_start_tick;
+	uint8_t countdown_seconds;
+	bool input_locked;
+} em_game_match_state_t;
+
+typedef struct
+{
+	uint8_t screen;
+	uint8_t state;
+	uint8_t round;
+	uint8_t goals;
+	uint8_t saves;
+	uint8_t misses;
+	uint8_t countdown;
+	uint8_t shooter_direction;
+	uint8_t keeper_direction;
+	uint8_t last_result;
+	uint8_t winner;
+	int16_t ball_x;
+	int16_t ball_y;
+	uint8_t ball_frame;
+	uint8_t keeper_frame;
+	uint8_t shooter_frame;
+	uint8_t flags;
+} em_game_view_t;
+
+static_assert(sizeof(em_game_view_t) <= AK_COMMON_MSG_DATA_SIZE, "em_game_view_t exceeds AK common message payload");
+
+extern void em_game_match_state_init(em_game_match_state_t* ms);
+extern void em_game_match_state_reset(em_game_match_state_t* ms);
+extern void em_game_match_state_build_view(const em_game_match_state_t* ms, em_game_view_t* view);
+
+#endif //__EM_GAME_MATCH_STATE_H__
