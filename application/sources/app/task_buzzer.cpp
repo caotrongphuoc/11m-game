90e8092c857177dd1090fcfe6884f2d226848f06
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-15 22:56:04 +0700

    [ADD] route em_game buzzer signals to melody player

--- application/sources/app/task_buzzer.cpp
+++ application/sources/app/task_buzzer.cpp
@@ -11,6 +11,36 @@ void task_buzzer(ak_msg_t* msg) {
 		BUZZER_PlaySound(BUZZER_SOUND_3BEEP);
 	} break;
 
+	case EM_GAME_BUZZER_KICK: {
+		APP_DBG_SIG("EM_GAME_BUZZER_KICK\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_KICK);
+	} break;
+
+	case EM_GAME_BUZZER_GOAL: {
+		APP_DBG_SIG("EM_GAME_BUZZER_GOAL\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_GOAL);
+	} break;
+
+	case EM_GAME_BUZZER_SAVE: {
+		APP_DBG_SIG("EM_GAME_BUZZER_SAVE\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_SAVE);
+	} break;
+
+	case EM_GAME_BUZZER_MISS: {
+		APP_DBG_SIG("EM_GAME_BUZZER_MISS\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_MISS);
+	} break;
+
+	case EM_GAME_BUZZER_WIN: {
+		APP_DBG_SIG("EM_GAME_BUZZER_WIN\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_WIN);
+	} break;
+
+	case EM_GAME_BUZZER_LOSE: {
+		APP_DBG_SIG("EM_GAME_BUZZER_LOSE\n");
+		BUZZER_PlaySound(BUZZER_SOUND_EM_GAME_LOSE);
+	} break;
+
 	default:
 		break;
 	}
