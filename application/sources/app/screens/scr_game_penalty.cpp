efaac65d07b413d8b51f680c04e824e7b5911682
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-16 16:09:05 +0700

    [UPDATE] enlarge penalty goal and draw field lines

--- application/sources/app/screens/scr_game_penalty.cpp
+++ application/sources/app/screens/scr_game_penalty.cpp
@@ -11,18 +11,24 @@
 #define SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_HEIGHT (14)
 #define SCR_GAME_PENALTY_SHOOTER_BITMAP_WIDTH (15)
 #define SCR_GAME_PENALTY_SHOOTER_BITMAP_HEIGHT (20)
-#define SCR_GAME_PENALTY_GOAL_BITMAP_WIDTH (128)
-#define SCR_GAME_PENALTY_GOAL_BITMAP_HEIGHT (32)
-#define SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_X (0)
-#define SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_Y (17)
+#define SCR_GAME_PENALTY_GOAL_BITMAP_WIDTH (93)
+#define SCR_GAME_PENALTY_GOAL_BITMAP_HEIGHT (38)
+#define SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_X (17)
+#define SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_Y (15)
+#define SCR_GAME_PENALTY_GOAL_LINE_AXIS_Y (53)
+#define SCR_GAME_PENALTY_BOX_TOP_LEFT_AXIS_X (16)
+#define SCR_GAME_PENALTY_BOX_TOP_RIGHT_AXIS_X (111)
+#define SCR_GAME_PENALTY_BOX_BOTTOM_LEFT_AXIS_X (28)
+#define SCR_GAME_PENALTY_BOX_BOTTOM_RIGHT_AXIS_X (99)
+#define SCR_GAME_PENALTY_BOX_BOTTOM_AXIS_Y (63)
 #define SCR_GAME_PENALTY_COUNTDOWN_TEXT_SIZE (2)
 #define SCR_GAME_PENALTY_COUNTDOWN_AXIS_X (58)
 #define SCR_GAME_PENALTY_COUNTDOWN_AXIS_Y (7)
 
 static void view_scr_game_penalty();
-static const unsigned char* scr_game_penalty_get_shooter_bitmap();
+static const unsigned char *scr_game_penalty_get_shooter_bitmap();
 
-static const unsigned char* scr_game_penalty_get_shooter_bitmap()
+static const unsigned char *scr_game_penalty_get_shooter_bitmap()
 {
 	if (em_game_shooter.frame == 0)
 	{
@@ -38,22 +44,22 @@ static const unsigned char* scr_game_penalty_get_shooter_bitmap()
 }
 
 view_dynamic_t dyn_view_game_penalty = {
-    {
-        .item_type = ITEM_TYPE_DYNAMIC,
-    },
-    view_scr_game_penalty};
+	{
+		.item_type = ITEM_TYPE_DYNAMIC,
+	},
+	view_scr_game_penalty};
 
 view_screen_t scr_game_penalty = {
-    &dyn_view_game_penalty,
-    ITEM_NULL,
-    ITEM_NULL,
+	&dyn_view_game_penalty,
+	ITEM_NULL,
+	ITEM_NULL,
 
-    .focus_item = 0,
+	.focus_item = 0,
 };
 
 void view_scr_game_penalty()
 {
-	const em_game_view_t* view = task_display_get_game_view();
+	const em_game_view_t *view = task_display_get_game_view();
 	uint8_t ai_score = view->saves + view->misses;
 
 	view_render.clear();
@@ -72,70 +78,78 @@ void view_scr_game_penalty()
 	view_render.print("/5");
 
 	if (view->state == EM_GAME_STATE_SHOOTER_WAIT &&
-	    view->countdown > 0)
+		view->countdown > 0)
 	{
 		view_render.setTextSize(SCR_GAME_PENALTY_COUNTDOWN_TEXT_SIZE);
 		view_render.setCursor(SCR_GAME_PENALTY_COUNTDOWN_AXIS_X,
-		                      SCR_GAME_PENALTY_COUNTDOWN_AXIS_Y);
+							  SCR_GAME_PENALTY_COUNTDOWN_AXIS_Y);
 		view_render.print((unsigned int)view->countdown);
 	}
 
 	view_render.drawBitmap(
-	    SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_X,
-	    SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_Y,
-	    bitmap_penalty_goal,
-	    SCR_GAME_PENALTY_GOAL_BITMAP_WIDTH,
-	    SCR_GAME_PENALTY_GOAL_BITMAP_HEIGHT,
-	    WHITE);
+		SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_X,
+		SCR_GAME_PENALTY_GOAL_BITMAP_AXIS_Y,
+		bitmap_penalty_goal,
+		SCR_GAME_PENALTY_GOAL_BITMAP_WIDTH,
+		SCR_GAME_PENALTY_GOAL_BITMAP_HEIGHT,
+		WHITE);
+
+	view_render.drawLine(0, SCR_GAME_PENALTY_GOAL_LINE_AXIS_Y, 127, SCR_GAME_PENALTY_GOAL_LINE_AXIS_Y, WHITE);
+	view_render.drawLine(SCR_GAME_PENALTY_BOX_TOP_LEFT_AXIS_X, SCR_GAME_PENALTY_GOAL_LINE_AXIS_Y,
+	                     SCR_GAME_PENALTY_BOX_BOTTOM_LEFT_AXIS_X, SCR_GAME_PENALTY_BOX_BOTTOM_AXIS_Y, WHITE);
+	view_render.drawLine(SCR_GAME_PENALTY_BOX_TOP_RIGHT_AXIS_X, SCR_GAME_PENALTY_GOAL_LINE_AXIS_Y,
+	                     SCR_GAME_PENALTY_BOX_BOTTOM_RIGHT_AXIS_X, SCR_GAME_PENALTY_BOX_BOTTOM_AXIS_Y, WHITE);
+	view_render.drawLine(SCR_GAME_PENALTY_BOX_BOTTOM_LEFT_AXIS_X, SCR_GAME_PENALTY_BOX_BOTTOM_AXIS_Y,
+	                     SCR_GAME_PENALTY_BOX_BOTTOM_RIGHT_AXIS_X, SCR_GAME_PENALTY_BOX_BOTTOM_AXIS_Y, WHITE);
 
 	if ((view->flags & EM_GAME_VIEW_FLAG_KEEPER_VISIBLE) != 0)
 	{
 		if (em_game_keeper.direction == EM_GAME_DIRECTION_NONE)
 		{
 			view_render.drawBitmap(
-			    view->keeper_x - (SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_WIDTH / 2),
-			    view->keeper_y,
-			    bitmap_penalty_keeper_idle,
-			    SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_WIDTH,
-			    SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_HEIGHT,
-			    WHITE);
+				view->keeper_x - (SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_WIDTH / 2),
+				view->keeper_y,
+				bitmap_penalty_keeper_idle,
+				SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_WIDTH,
+				SCR_GAME_PENALTY_KEEPER_IDLE_BITMAP_HEIGHT,
+				WHITE);
 		}
 		else
 		{
 			view_render.drawBitmap(
-			    view->keeper_x - (SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_WIDTH / 2),
-			    view->keeper_y,
-			    bitmap_penalty_keeper_dive,
-			    SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_WIDTH,
-			    SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_HEIGHT,
-			    WHITE);
+				view->keeper_x - (SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_WIDTH / 2),
+				view->keeper_y,
+				bitmap_penalty_keeper_dive,
+				SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_WIDTH,
+				SCR_GAME_PENALTY_KEEPER_DIVE_BITMAP_HEIGHT,
+				WHITE);
 		}
 	}
 
 	if ((view->flags & EM_GAME_VIEW_FLAG_BALL_VISIBLE) != 0)
 	{
 		view_render.drawBitmap(
-		    view->ball_x - (SCR_GAME_PENALTY_BALL_BITMAP_WIDTH / 2),
-		    view->ball_y - (SCR_GAME_PENALTY_BALL_BITMAP_HEIGHT / 2),
-		    bitmap_penalty_ball,
-		    SCR_GAME_PENALTY_BALL_BITMAP_WIDTH,
-		    SCR_GAME_PENALTY_BALL_BITMAP_HEIGHT,
-		    WHITE);
+			view->ball_x - (SCR_GAME_PENALTY_BALL_BITMAP_WIDTH / 2),
+			view->ball_y - (SCR_GAME_PENALTY_BALL_BITMAP_HEIGHT / 2),
+			bitmap_penalty_ball,
+			SCR_GAME_PENALTY_BALL_BITMAP_WIDTH,
+			SCR_GAME_PENALTY_BALL_BITMAP_HEIGHT,
+			WHITE);
 	}
 
 	if ((view->flags & EM_GAME_VIEW_FLAG_SHOOTER_VISIBLE) != 0)
 	{
 		view_render.drawBitmap(
-		    view->shooter_x - (SCR_GAME_PENALTY_SHOOTER_BITMAP_WIDTH / 2),
-		    view->shooter_y - SCR_GAME_PENALTY_SHOOTER_BITMAP_HEIGHT,
-		    scr_game_penalty_get_shooter_bitmap(),
-		    SCR_GAME_PENALTY_SHOOTER_BITMAP_WIDTH,
-		    SCR_GAME_PENALTY_SHOOTER_BITMAP_HEIGHT,
-		    WHITE);
+			view->shooter_x - (SCR_GAME_PENALTY_SHOOTER_BITMAP_WIDTH / 2),
+			view->shooter_y - SCR_GAME_PENALTY_SHOOTER_BITMAP_HEIGHT,
+			scr_game_penalty_get_shooter_bitmap(),
+			SCR_GAME_PENALTY_SHOOTER_BITMAP_WIDTH,
+			SCR_GAME_PENALTY_SHOOTER_BITMAP_HEIGHT,
+			WHITE);
 	}
 }
 
-void scr_game_penalty_handle(ak_msg_t* msg)
+void scr_game_penalty_handle(ak_msg_t *msg)
 {
 	switch (msg->sig)
 	{
